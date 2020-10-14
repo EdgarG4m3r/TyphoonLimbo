81e527f061f66647ed1a1fd6ef4085145f1486ce
EdgarG4m3r <killzone551@ymail.com>
2020-10-14 21:40:08 +0800

    Update limbo.go

--- limbo.go
+++ limbo.go
@@ -24,25 +24,8 @@ func main() {
 		if config.JoinMessage != nil {
 			e.Player.SendRawMessage(string(config.JoinMessage))
 		}
-		if &bossbarCreate != nil {
-			e.Player.WritePacket(&bossbarCreate)
-		}
-		if &playerListHF != nil {
-			e.Player.WritePacket(&playerListHF)
-		}
 	})
-
-	core.On(func(e *t.PlayerChatEvent) {
-		msg := t.ChatMessage("")
-		msg.SetExtra([]t.IChatComponent{
-			t.ChatMessage("<"),
-			t.ChatMessage(e.Player.GetName()),
-			t.ChatMessage("> "),
-			t.ChatMessage(e.Message),
-		})
-		e.Player.SendMessage(msg)
-	})
-
+	
 	core.Start()
 }
 
